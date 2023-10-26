e75646f9ad9c8717311c5c61f91ea19ff3b982b7
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2023-10-26 01:13:24 -0300

    Update rosto.js

--- rosto.js
+++ rosto.js
@@ -1,12 +1,6 @@
 // Obtém a URL completa da página
 var urlCompleta = window.location.href;
 
-// Divide a URL em partes usando '/' como separador
-var partesDaUrl = urlCompleta.split('/');
-
-// Obtém o segundo elemento do path (índice 1)
-var segundoElementoDoPath = partesDaUrl[1];
-
 // Obtém a data e hora atual no formato yyyymmhhddmm
 var dataHora = new Date();
 var ano = dataHora.getFullYear();
@@ -16,8 +10,11 @@ var hora = dataHora.getHours().toString().padStart(2, '0');
 var minuto = dataHora.getMinutes().toString().padStart(2, '0');
 var dataHoraFormatada = ano + mes + dia + hora + minuto;
 
+var parametros = new URLSearchParams(urlCompleta);
+var raValor = parametros.get('ra');
+
 // Cria a nova URL
-var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + segundoElementoDoPath + "&p_aut=" + dataHoraFormatada;
+var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + raValor + "&p_aut=" + dataHoraFormatada;
 
 // Redireciona para a nova URL
 window.location.href = novaUrl;
