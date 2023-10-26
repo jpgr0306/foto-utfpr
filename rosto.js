53e6e3fc471f62e7bc652b68d8543237035d5152
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2023-10-26 01:18:34 -0300

    Update rosto.js

--- rosto.js
+++ rosto.js
@@ -1,20 +1,34 @@
 // Obtém a URL completa da página
 var urlCompleta = window.location.href;
 
-// Obtém a data e hora atual no formato yyyymmhhddmm
-var dataHora = new Date();
-var ano = dataHora.getFullYear();
-var mes = (dataHora.getMonth() + 1).toString().padStart(2, '0');
-var dia = dataHora.getDate().toString().padStart(2, '0');
-var hora = dataHora.getHours().toString().padStart(2, '0');
-var minuto = dataHora.getMinutes().toString().padStart(2, '0');
-var dataHoraFormatada = ano + mes + dia + hora + minuto;
+// Função para obter o valor de um parâmetro da URL
+function obterParametroDaURL(nomeParametro) {
+    var parametros = new URLSearchParams(window.location.search);
+    return parametros.get(nomeParametro);
+}
 
-var parametros = new URLSearchParams(urlCompleta);
-var raValor = parametros.get('ra');
+// Obtém o valor do parâmetro 'ra'
+var raValor = obterParametroDaURL('ra');
 
-// Cria a nova URL
-var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + raValor + "&p_aut=" + dataHoraFormatada;
+if (raValor) {
+    // Remove o último dígito do valor de 'ra'
+    raValor = raValor.slice(0, -1);
 
-// Redireciona para a nova URL
-window.location.href = novaUrl;
+    // Obtém a data e hora atual no formato yyyymmddhhmm
+    var dataHora = new Date();
+    var ano = dataHora.getFullYear();
+    var mes = (dataHora.getMonth() + 1).toString().padStart(2, '0');
+    var dia = dataHora.getDate().toString().padStart(2, '0');
+    var hora = dataHora.getHours().toString().padStart(2, '0');
+    var minuto = dataHora.getMinutes().toString().padStart(2, '0');
+    var dataHoraFormatada = ano + mes + dia + hora + minuto;
+
+    // Cria a nova URL
+    var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + raValor + "&p_aut=" + dataHoraFormatada;
+
+    // Redireciona para a nova URL
+    window.location.href = novaUrl;
+} else {
+    // Se o parâmetro 'ra' não for encontrado na URL, faça algo aqui, como exibir uma mensagem de erro.
+    console.log("Parâmetro 'ra' não encontrado na URL.");
+}
