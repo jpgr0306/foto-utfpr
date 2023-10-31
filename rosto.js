426d5027d616f8afca21595b22215ac0b0b59eb4
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2023-10-30 22:33:19 -0300

    Update rosto.js

--- rosto.js
+++ rosto.js
@@ -1,40 +1,34 @@
-function redirecionarAteNaoExpirar() {
-    var raValor = obterParametroDaURL('ra');
-    
-    if (raValor) {
-        raValor = raValor.slice(0, -1);
+// Obtém a URL completa da página
+var urlCompleta = window.location.href;
 
-        function redirecionar() {
-            var dataHora = new Date();
-            var ano = dataHora.getFullYear();
-            var mes = (dataHora.getMonth() + 1).toString().padStart(2, '0');
-            var dia = dataHora.getDate().toString().padStart(2, '0');
-            var hora = dataHora.getHours().toString().padStart(2, '0');
-            var minuto = dataHora.getMinutes().toString().padStart(2, '0');
-            var dataHoraFormatada = ano + mes + dia + hora + minuto;
-            
-            var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + raValor + "&p_aut=" + dataHoraFormatada;
-            window.location.href = novaUrl;
-        }
-        
-        var intervalo = setInterval(function () {
-            fetch(novaUrl)
-                .then(function (response) {
-                    return response.text();
-                })
-                .then(function (text) {
-                    if (text.trim() !== "A url expirou.") {
-                        clearInterval(intervalo);
-                        redirecionar();
-                    }
-                })
-                .catch(function (error) {
-                    console.log("Erro na verificação: " + error);
-                });
-        }, 1000); // Verifique a cada segundo (você pode ajustar o intervalo conforme necessário)
-    } else {
-        console.log("Parâmetro 'ra' não encontrado na URL.");
-    }
+// Função para obter o valor de um parâmetro da URL
+function obterParametroDaURL(nomeParametro) {
+    var parametros = new URLSearchParams(window.location.search);
+    return parametros.get(nomeParametro);
 }
 
-redirecionarAteNaoExpirar();
+// Obtém o valor do parâmetro 'ra'
+var raValor = obterParametroDaURL('ra');
+
+if (raValor) {
+    // Remove o último dígito do valor de 'ra'
+    raValor = raValor.slice(0, -1);
+
+    // Obtém a data e hora atual no formato yyyymmddhhmm
+    var dataHora = new Date();
+    var ano = dataHora.getFullYear();
+    var mes = (dataHora.getMonth() + 1).toString().padStart(2, '0');
+    var dia = dataHora.getDate().toString().padStart(2, '0');
+    var hora = dataHora.getHours().toString().padStart(2, '0');
+    var minuto = dataHora.getMinutes().toString().padStart(2, '0');
+    var dataHoraFormatada = ano + mes + dia + hora + minuto;
+
+    // Cria a nova URL
+    var novaUrl = "https://sistemas2.utfpr.edu.br/dpls/sistema/aluno01/mpFoto.pcImagem?p_pesscodnr=" + raValor + "&p_aut=" + dataHoraFormatada;
+
+    // Redireciona para a nova URL
+    window.location.href = novaUrl;
+} else {
+    // Se o parâmetro 'ra' não for encontrado na URL, faça algo aqui, como exibir uma mensagem de erro.
+    console.log("Parâmetro 'ra' não encontrado na URL.");
+}
